5e424992954ae99304b4ae34158f82244d3ccb5e
Florian Porsch <tederean@gmail.com>
2024-07-06 19:09:58 +0200

    Optional serde support

---
+++ src/no_serde_support.rs
@@ -0,0 +1,510 @@
+use core::borrow::Borrow;
+use core::convert::AsRef;
+use core::fmt;
+use core::marker::PhantomData;
+use core::ops::{
+    Add, BitAnd, BitOr, BitXor, Bound, Div, Index, Mul, Neg, Not, RangeBounds, Rem, Shl, Shr, Sub,
+};
+
+/// A `Predicate` tests if a value satisfies a particular refinement type.
+///
+/// Used in conjunction with [`Refinement`](self::Refinement).
+///
+/// # Example
+///
+/// ```
+/// use refinement::Predicate;
+///
+/// struct LessThanTen;
+///
+/// impl Predicate<i32> for LessThanTen {
+///     fn test(x: &i32) -> bool {
+///         *x < 10
+///     }
+/// }
+///
+/// ```
+pub trait Predicate<T> {
+    /// Test if a value satisfies the `Predicate`.
+    ///
+    /// See [`Refinement`](Refinement) for usage examples.
+    fn test(x: &T) -> bool;
+}
+
+/// A `Refinement` type ensures all values of a particular type satisfy a [`Predicate`].
+///
+/// Use [`as_inner`](Refinement::as_inner)/[`to_inner`](Refinement::to_inner) to access the
+/// underlying value or [`into_inner`](Refinement::into_inner) to unwrap the value.
+///
+/// `Refinement` also implements many common standard library traits if the underlying
+/// value also implements them.
+///
+/// # Examples
+/// ```
+/// use refinement::{Predicate, Refinement};
+///
+/// struct LessThanTen;
+///
+/// impl Predicate<i32> for LessThanTen {
+///     fn test(x: &i32) -> bool {
+///         *x < 10
+///     }
+/// }
+///
+/// type LessThanTenInt = Refinement<i32, LessThanTen>;
+///
+/// let x = LessThanTenInt::new(5);
+/// assert!(x.is_some());
+///
+/// let y = LessThanTenInt::new(11);
+/// assert!(y.is_none());
+/// ```
+#[derive(PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Refinement<T, P>(T, PhantomData<P>);
+
+impl<T, P> Refinement<T, P>
+    where
+        P: Predicate<T>,
+{
+    /// Create a refined value from the underlying type `T`.
+    ///
+    /// Returns `x` under the refinement type if `x` satisfies `P`, otherwise returns [`None`](core::option::Option::None).
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use refinement::{Predicate, Refinement};
+    ///
+    /// struct NonEmpty;
+    ///
+    /// impl Predicate<String> for NonEmpty {
+    ///     fn test(x: &String) -> bool {
+    ///        !x.is_empty()
+    ///     }
+    /// }
+    ///
+    /// type NonEmptyString = Refinement<String, NonEmpty>;
+    ///
+    /// let s1 = NonEmptyString::new(String::from("Hello"));
+    /// assert!(s1.is_some());
+    ///
+    /// let s2 = NonEmptyString::new(String::from(""));
+    /// assert!(s2.is_none());
+    /// ```
+    pub fn new(x: T) -> Option<Self> {
+        if P::test(&x) {
+            Some(Refinement(x, PhantomData))
+        } else {
+            None
+        }
+    }
+
+    /// Unwrap the underlying value, consuming `self`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use refinement::{Predicate, Refinement};
+    ///
+    /// struct ThreeDigit;
+    ///
+    /// impl Predicate<String> for ThreeDigit {
+    ///     fn test(x: &String) -> bool {
+    ///        x.chars().count() == 3 && x.chars().filter(|c| c.is_ascii_digit()).count() == 3
+    ///     }
+    /// }
+    ///
+    /// type ThreeDigitString = Refinement<String, ThreeDigit>;
+    ///
+    /// let s = ThreeDigitString::new(String::from("123"));
+    ///
+    /// assert_eq!(String::from("123"), s.unwrap().into_inner());
+    /// ```
+    pub fn into_inner(self) -> T {
+        self.0
+    }
+}
+
+impl<T, P> fmt::Debug for Refinement<T, P>
+    where
+        T: fmt::Debug
+{
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_tuple("Refinement")
+            .field(&self.0) // : T
+            .field(&format_args!("_")) // : PhantomData<P>
+            .finish()
+    }
+}
+
+impl<T: Clone, P> Clone for Refinement<T, P> {
+    fn clone(&self) -> Self {
+        Self(self.0.clone(), self.1.clone())
+    }
+}
+
+impl<T: Copy, P> Copy for Refinement<T, P> {}
+
+impl<T, P> core::ops::Deref for Refinement<T, P> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T, P> Add<T> for Refinement<T, P>
+    where
+        T: Add<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn add(self, rhs: T) -> Self::Output {
+        Self::new(self.0 + rhs)
+    }
+}
+
+impl<T, P> Add for Refinement<T, P>
+    where
+        T: Add<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(self.0 + rhs.0)
+    }
+}
+
+impl<T, P> BitAnd<T> for Refinement<T, P>
+    where
+        T: BitAnd<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn bitand(self, rhs: T) -> Self::Output {
+        Self::new(self.0 & rhs)
+    }
+}
+
+impl<T, P> BitAnd for Refinement<T, P>
+    where
+        T: BitAnd<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn bitand(self, rhs: Self) -> Self::Output {
+        Self::new(self.0 & rhs.0)
+    }
+}
+
+impl<T, P> BitOr<T> for Refinement<T, P>
+    where
+        T: BitOr<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn bitor(self, rhs: T) -> Self::Output {
+        Self::new(self.0 | rhs)
+    }
+}
+
+impl<T, P> BitOr for Refinement<T, P>
+    where
+        T: BitOr<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn bitor(self, rhs: Self) -> Self::Output {
+        Self::new(self.0 | rhs.0)
+    }
+}
+
+impl<T, P> BitXor<T> for Refinement<T, P>
+    where
+        T: BitXor<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn bitxor(self, rhs: T) -> Self::Output {
+        Self::new(self.0 ^ rhs)
+    }
+}
+
+impl<T, P> BitXor for Refinement<T, P>
+    where
+        T: BitXor<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn bitxor(self, rhs: Self) -> Self::Output {
+        Self::new(self.0 ^ rhs.0)
+    }
+}
+
+impl<T, P> Div<T> for Refinement<T, P>
+    where
+        T: Div<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn div(self, rhs: T) -> Self::Output {
+        Self::new(self.0 / rhs)
+    }
+}
+
+impl<T, P> Div for Refinement<T, P>
+    where
+        T: Div<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn div(self, rhs: Self) -> Self::Output {
+        Self::new(self.0 / rhs.0)
+    }
+}
+
+impl<T, P> Mul<T> for Refinement<T, P>
+    where
+        T: Mul<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn mul(self, rhs: T) -> Self::Output {
+        Self::new(self.0 * rhs)
+    }
+}
+
+impl<T, P> Mul for Refinement<T, P>
+    where
+        T: Mul<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self::new(self.0 * rhs.0)
+    }
+}
+
+impl<T, P> Neg for Refinement<T, P>
+    where
+        T: Neg<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn neg(self) -> Self::Output {
+        Self::new(self.0.neg())
+    }
+}
+
+impl<T, P> Not for Refinement<T, P>
+    where
+        T: Not<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn not(self) -> Self::Output {
+        Self::new(self.0.not())
+    }
+}
+
+impl<T, P> Rem<T> for Refinement<T, P>
+    where
+        T: Rem<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn rem(self, rhs: T) -> Self::Output {
+        Self::new(self.0 % rhs)
+    }
+}
+
+impl<T, P> Rem for Refinement<T, P>
+    where
+        T: Rem<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn rem(self, rhs: Self) -> Self::Output {
+        Self::new(self.0 % rhs.0)
+    }
+}
+
+impl<T, P> Shl<T> for Refinement<T, P>
+    where
+        T: Shl<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn shl(self, rhs: T) -> Self::Output {
+        Self::new(self.0 << rhs)
+    }
+}
+
+impl<T, P> Shl for Refinement<T, P>
+    where
+        T: Shl<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn shl(self, rhs: Self) -> Self::Output {
+        Self::new(self.0 << rhs.0)
+    }
+}
+
+impl<T, P> Shr<T> for Refinement<T, P>
+    where
+        T: Shr<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn shr(self, rhs: T) -> Self::Output {
+        Self::new(self.0 >> rhs)
+    }
+}
+
+impl<T, P> Shr for Refinement<T, P>
+    where
+        T: Shr<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn shr(self, rhs: Self) -> Self::Output {
+        Self::new(self.0 >> rhs.0)
+    }
+}
+
+impl<T, P> Sub<T> for Refinement<T, P>
+    where
+        T: Sub<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn sub(self, rhs: T) -> Self::Output {
+        Self::new(self.0 - rhs)
+    }
+}
+
+impl<T, P> Sub for Refinement<T, P>
+    where
+        T: Sub<Output = T>,
+        P: Predicate<T>,
+{
+    type Output = Option<Self>;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::new(self.0 - rhs.0)
+    }
+}
+
+impl<T, P> fmt::Display for Refinement<T, P>
+    where
+        T: fmt::Display,
+        P: Predicate<T>,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl<T, P> crate::Refinement<T, P>
+    where
+        T: Clone,
+        P: crate::Predicate<T>,
+{
+    /// Retrieve the underlying value without consuming `self`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use refinement::{Predicate, Refinement};
+    ///
+    /// struct ThreeDigit;
+    ///
+    /// impl Predicate<String> for ThreeDigit {
+    ///     fn test(x: &String) -> bool {
+    ///        x.chars().count() == 3 && x.chars().filter(|c| c.is_ascii_digit()).count() == 3
+    ///     }
+    /// }
+    ///
+    /// type ThreeDigitString = Refinement<String, ThreeDigit>;
+    ///
+    /// let s = ThreeDigitString::new(String::from("123"));
+    ///
+    /// assert_eq!(&String::from("123"), s.unwrap().to_inner());
+    /// ```
+    pub fn to_inner(&self) -> &T {
+        &self.0
+    }
+}
+
+impl<T, P> crate::Refinement<T, P>
+    where
+        T: Copy,
+        P: crate::Predicate<T>,
+{
+    /// Retrieve the underlying value for [`Copy`] types without consuming `self`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use refinement::{Predicate, Refinement};
+    ///
+    /// struct LessThanTen;
+    ///
+    /// impl Predicate<i32> for LessThanTen {
+    ///     fn test(x: &i32) -> bool {
+    ///         *x < 10
+    ///     }
+    /// }
+    ///
+    /// type LessThanTenInt = Refinement<i32, LessThanTen>;
+    ///
+    /// let x = LessThanTenInt::new(5);
+    /// assert_eq!(5, x.unwrap().as_inner());
+    /// ```
+    pub fn as_inner(&self) -> T {
+        self.0
+    }
+}
+
+impl<T, P> Borrow<T> for crate::Refinement<T, P>
+    where
+        P: crate::Predicate<T>,
+{
+    fn borrow(&self) -> &T {
+        &self.0
+    }
+}
+
+impl<T, P> AsRef<T> for crate::Refinement<T, P>
+    where
+        P: crate::Predicate<T>,
+{
+    fn as_ref(&self) -> &T {
+        &self.0
+    }
+}
+
+impl<T, P, I> Index<I> for crate::Refinement<T, P>
+    where
+        T: Index<I>,
+        P: crate::Predicate<T>,
+{
+    type Output = T::Output;
+    fn index(&self, index: I) -> &Self::Output {
+        self.0.index(index)
+    }
+}
+
+impl<T, P, B> RangeBounds<B> for crate::Refinement<T, P>
+    where
+        T: RangeBounds<B>,
+        P: crate::Predicate<T>,
+{
+    fn start_bound(&self) -> Bound<&B> {
+        self.0.start_bound()
+    }
+
+    fn end_bound(&self) -> Bound<&B> {
+        self.0.end_bound()
+    }
+}
